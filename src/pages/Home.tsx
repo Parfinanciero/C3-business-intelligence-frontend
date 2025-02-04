54553435c1548c8b7d6592928d5ed670694ac7fc
Julian Sneider <juliansnei@gmail.com>
2025-02-03 21:17:33 -0500

    Creating the category chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Header from "../components/common/Header";
-import BalanceChartCircle from "../components/expensesAndIncomes/BalanceChartCircle";
-import BalanceChartBar from "../components/expensesAndIncomes/BalanceChartBar";
-import SimpleBalanceCard from "../components/expensesAndIncomes/BalanceSheetCard";
+import BalanceChartCircle from "../components/Home/BalanceChartCircle";
+import BalanceChartBar from "../components/Home/BalanceChartBar";
+import SimpleBalanceCard from "../components/Home/BalanceSheetCard";
+import CategoryDistributionChart from "../components/Home/CategoryIncomeChart";
 
 interface ExpenseData {
   totalExpenses: number;
@@ -13,9 +14,15 @@ interface IncomeData {
   totalExpenses: number;
 }
 
+interface CategoryData {
+  category: string;
+  percentage: number;
+}
+
 const Home: React.FC = () => {
   const [expenseData, setExpenseData] = useState<ExpenseData | null>(null);
   const [incomeData, setIncomeData] = useState<IncomeData | null>(null);
+  const [categoryExpenseData, setCategoryData] = useState<CategoryData[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -32,15 +39,21 @@ const Home: React.FC = () => {
       try {
         setLoading(true);
 
-        const expenseResponse = await fetch("http://23.88.104.53:8081/api/finanzas/gastos/1/01");
+        const expenseResponse = await fetch("http://localhost:8081/api/finanzas/gastos/1/01");
         if (!expenseResponse.ok) throw new Error(`Error al obtener los gastos: ${expenseResponse.status}`);
         const expenseData: ExpenseData = await expenseResponse.json();
         setExpenseData(expenseData);
 
-        const incomeResponse = await fetch("http://23.88.104.53:8081/api/finanzas/ingresos/1/01");
+        const incomeResponse = await fetch("http://localhost:8081/api/finanzas/ingresos/1/01");
         if (!incomeResponse.ok) throw new Error(`Error al obtener los ingresos: ${incomeResponse.status}`);
         const incomeData: IncomeData = await incomeResponse.json();
         setIncomeData(incomeData);
+
+        const expenseCategoryResponse = await fetch("http://localhost:8081/api/finanzas/metricas/gastos-categoria/1/01/");
+        if (!expenseCategoryResponse.ok) throw new Error(`Error al obtener las categorias: ${expenseCategoryResponse.status}`);
+        const categoryExpenseData: CategoryData[] = await expenseCategoryResponse.json();
+        setCategoryData(categoryExpenseData);
+
       } catch (err: any) {
         setError(err.message || "Error desconocido");
       } finally {
@@ -57,44 +70,49 @@ const Home: React.FC = () => {
   return (
     <div className='flex-1 overflow-auto relative z-10'>
       <Header title="Parfinanciero" />
-      <motion.div
-        className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.3 }}
-      >
-        <h1>Bienvenido a Parfinanciero</h1>
+      <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
         
-        {expenseData ? (
-          <div>
-            <h2>Total de Gastos: ${expenseData.totalExpenses.toFixed(2)}</h2>
-          </div>
-        ) : (
-          <p>No hay datos disponibles.</p>
-        )}
 
-        {incomeData ? (
-          <div>
-            <h2>Total de Ingresos: ${incomeData.totalExpenses.toFixed(2)}</h2>
-          </div>
-        ) : (
-          <p>No hay datos de ingresos disponibles.</p>
-        )}
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+        <motion.div
+          className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.3 }}
+        >
+          <h1>Bienvenido a Parfinanciero</h1>
+          
+          {expenseData ? (
+            <div>
+              <h2>Total de Gastos: ${expenseData.totalExpenses.toFixed(2)}</h2>
+            </div>
+          ) : (
+            <p>No hay datos disponibles.</p>
+          )}
 
-        {/* Renderizado del Balance General Calculado */}
-        {calculateBalance() !== null && (
-          <SimpleBalanceCard balanceSheet={`${calculateBalance()}`} />
-        )}
-      </motion.div>
+          {incomeData ? (
+            <div>
+              <h2>Total de Ingresos: ${incomeData.totalExpenses.toFixed(2)}</h2>
+            </div>
+          ) : (
+            <p>No hay datos de ingresos disponibles.</p>
+          )}
 
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-        {incomeData && expenseData && (
-          <>
-            <BalanceChartCircle income={incomeData.totalExpenses} expense={expenseData.totalExpenses} />
-            <BalanceChartBar income={incomeData.totalExpenses} expense={expenseData.totalExpenses} />
-          </>
-        )}
-      </div>
+          {/* Renderizado del Balance General Calculado */}
+          {calculateBalance() !== null && (
+            <SimpleBalanceCard balanceSheet={`${calculateBalance()}`} />
+          )}
+        </motion.div>
+          {incomeData && expenseData && (
+            <>
+              <BalanceChartCircle income={incomeData.totalExpenses} expense={expenseData.totalExpenses} />
+              <BalanceChartBar income={incomeData.totalExpenses} expense={expenseData.totalExpenses} />
+              <CategoryDistributionChart categories={categoryExpenseData}/>
+            </>
+          )}
+        </div>
+      </main>
+      
     </div>
   );
 };
