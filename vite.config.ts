35b1e8be59fe9a79e4601045ccf3dd115ee8b8de
alejandro-velasquez <alejandroalzate.ava@gmail.com>
2025-02-06 23:07:56 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,23 @@ import { federation } from '@module-federation/vite';
 import { dependencies } from './package.json'; // Asegúrate de tener este archivo disponible
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Permite que el servidor sea accesible externamente
+    port: 8045, // Asegura que está en el puerto correcto
+    cors: {
+      origin: 'https://auth.parfinanciero.crudzaso.com', // Permite solicitudes desde el frontend remoto
+      methods: 'GET, POST, OPTIONS',
+      credentials: true,
+    },
+    proxy: {
+      '/remoteEntry.js': {
+        target: 'https://auth.parfinanciero.crudzaso.com',
+        changeOrigin: true,
+        secure: false, // Si hay problemas con HTTPS, prueba con `secure: true`
+      },
+    },
+  },
+
   plugins: [
     react(),
     federation({
@@ -24,5 +41,10 @@ export default defineConfig({
 
   build: {
     target: 'chrome89',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Mejora la carga de los módulos federados
+      },
+    },
   },
 });
