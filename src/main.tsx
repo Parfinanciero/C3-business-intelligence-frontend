0c89cd661d6033c40147418402948e4e879325bb
alejandro-velasquez <alejandroalzate.ava@gmail.com>
2025-02-07 02:29:41 -0500

    update:commented code

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from 'remote/AuthContext';
+// import AuthProvider from 'remote/AuthContext';
 
 // Asegúrate de que el elemento con id 'root' está presente en el DOM
 const rootElement = document.getElementById('root') as HTMLElement;
@@ -13,9 +13,9 @@ createRoot(rootElement).render(
   <StrictMode>
     
     <BrowserRouter>
-      <AuthProvider>
+      {/* <AuthProvider> */}
         <App />
-      </AuthProvider>
+      {/* </AuthProvider> */}
     </BrowserRouter>
   </StrictMode>
 );
